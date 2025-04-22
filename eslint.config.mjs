3b6e8aa7a41373c9222346d150dc2a0e9ab6d7c8
wwknt0782 <204444020+wwknt0782@users.noreply.github.com>
2025-04-23 08:55:44 +0900

    rev1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
+//const eslintConfig = [
+//  ...compat.extends("next/core-web-vitals", "next/typescript"),
+//];
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.config({
+        extends: ["next"],
+        rules: {
+            "@typescript-eslint/no-unused-vars": "off",
+        },
+    }),
 ];
 
 export default eslintConfig;
