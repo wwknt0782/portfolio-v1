c9259b30902df74438a6a88c667e71f5e4db283f
wwknt0782 <204444020+wwknt0782@users.noreply.github.com>
2025-05-08 14:44:51 +0900

    ページ遷移アニメーション追加

--- src/constants/PageList.tsx
+++ src/constants/PageList.tsx
@@ -4,3 +4,10 @@ export const PageList = {
     works: "制作実績",
     contact: "お問い合わせ",
 };
+
+export const LinkColor = {
+    "/": "bg-[var(--color-home-primary)]",
+    "/profile": "bg-[var(--color-profile-primary)]",
+    "/works": "bg-[var(--color-works-primary)]",
+    "/contact": "bg-[var(--color-contact-primary)]",
+};
