320bc5c507b10c650fbbfc9dbc327b7bb4627d77
wwknt0782 <204444020+wwknt0782@users.noreply.github.com>
2025-06-02 17:14:55 +0900

    アニメーション時間調整

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -42,7 +42,7 @@ export const AnimatedLink = ({ href, children, className }: Props) => {
             startAnimation();
             await setTimeout(() => {
                 router.push(href); // ページ遷移
-            }, 300); // 500ms後に遷移
+            }, 500); // 500ms後に遷移
             await setTimeout(() => {
                 endAnimation(); // アニメーション終了
             }, 2000);
@@ -61,7 +61,7 @@ export const AnimatedLink = ({ href, children, className }: Props) => {
 //
 //ページ遷移アニメーション
 export const PageTransitionAnimation = () => {
-    const time = 1.5; //秒後にアニメーション終了
+    const time = 1.2; //秒後にアニメーション終了
     const { isAnimating } = usePageTransition();
 
     return (
@@ -70,11 +70,11 @@ export const PageTransitionAnimation = () => {
                 <div>
                     <motion.div
                         className="fixed inset-0 w-[150vw] flex items-center justify-center z-800
-            bg-[#ceebff] dark:bg-[var(--color-home-dark-secondary)]"
+            bg-[#ceebff] dark:bg-[var(--color-home-dark-secondary)] ease-in-out"
                         animate={{ x: [3000, 0, 0, -3000] }}
                         transition={{
                             duration: time,
-                            times: [0, 0.3, 0.5, 1],
+                            times: [0, 0.25, 0.75, 1],
                         }}
                     ></motion.div>
                 </div>
